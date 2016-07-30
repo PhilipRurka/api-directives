f631557f9b48e50989c66fdc571cf42671a6daab
Philip Rurka <philiprurka@gmail.com>
2016-07-30 17:32:44 -0300

    ptt

--- scripts/controllers/apiCtrl.js
+++ scripts/controllers/apiCtrl.js
@@ -3,9 +3,11 @@
 angular.module('ApiTesting')
 .controller('apiCtrl', function($scope){
 
-   var weather =  require('../api/weather.js');
-   var postCode = process.argv.slice(2);
-   weather.getPostCode(postCode);
+
 
    console.log(weather.currently);
 });
+
+var weather =  require('../api/weather.js');
+var postCode = process.argv.slice(2);
+weather.getPostCode(postCode);
