bc8613f254ed8feb98729c63f7e29e4f275f62a5
Philip Rurka <philiprurka@gmail.com>
2016-07-30 16:25:37 -0300

    pushing to test

--- scripts/directives/first.js
+++ scripts/directives/first.js
@@ -3,7 +3,5 @@ angular.module('ContentPlugin')
 .directive('first', function() {
    return {
         templateUrl: 'templates/first.html',
-        controller: 'ApiTesting',
-        replace: true
     };
 })
